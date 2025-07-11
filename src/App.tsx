3ab5be4c4e1532e8b1bb94f01cf59435f7cd76c9
Abisoye Oke-Lawal <abiso.lawal@gmail.com>
2025-07-11 16:55:15 +0100

    - Apply theme and update containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { createTheme, ThemeProvider } from '@mui/material';
 import './App.css';
 import FlightContainer from './containers/FlightContainer';
+import { palette } from './theme';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 function App() {
+  const theme = createTheme({
+    palette,
+  });
   return (
-    <div>
-      <FlightContainer />
-    </div>
+    <ThemeProvider theme={theme}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <FlightContainer />
+      </LocalizationProvider>
+    </ThemeProvider>
   );
 }
 
--- src/containers/FlightContainer.tsx
+++ src/containers/FlightContainer.tsx
@@ -1,9 +1,15 @@
-import Header from '@/components/features/flight/header/Header';
+import Header from '@/components/features/flight/Header/Header';
+import SearchBar from '@/components/features/flight/SearchBar/SearchBar';
+import { Box, Stack, useMediaQuery } from '@mui/material';
 
 export default function FlightContainer() {
+  const isSmallScreen = useMediaQuery('(max-width:320px)');
   return (
-    <div>
-      <Header />
-    </div>
+    <Box>
+      <Header sx={{ marginBottom: isSmallScreen ? '10px' : '40px' }} />
+      <Stack sx={{ margin: 'auto', maxWidth: '1024px' }}>
+        <SearchBar onExplore={() => {}} />
+      </Stack>
+    </Box>
   );
 }
